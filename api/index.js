dc6f9fa70193c259a5a3ede6bb40afd5b607c15b
praveen1221 <praveenmalla1221@gmail.com>
2023-12-23 15:30:59 +0530

    connect to mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Mongo Success")
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello, MERN!');
+  });
 
 app.listen(3000,()=>{
     console.log('server running on port 3000')
