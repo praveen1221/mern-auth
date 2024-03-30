b78b413f94025ecd12221874b104e9752d80f396
praveen1221 <praveenmalla1221@gmail.com>
2024-03-30 18:00:05 +0530

    completed Update User Functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,9 +3,12 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { app } from "../firebase"
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice'
+import { useNavigate } from 'react-router-dom';
+
 export default function OAuth({ loader }) {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const handleGoogleClick = async () => {
         try {
@@ -26,6 +29,7 @@ export default function OAuth({ loader }) {
 
             const data = await res.json()
             dispatch(signInSuccess(data))
+            navigate('/')
         }
         catch (err) {
             console.log("could not login with google", err);
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRef, useState } from 'react';
 import {
   getDownloadURL,
@@ -7,18 +7,19 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const [image, setImage] = useState(undefined);
+  const dispatch = useDispatch()
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false)
 
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loader, errorMsg } = useSelector((state) => state.user);
 
   const handleFileUpload = async (image) => {
-    setImage(image)
     const storage = getStorage(app);
     const fileName = new Date().getTime() + image.name;
     const storageRef = ref(storage, fileName);
@@ -40,10 +41,41 @@ export default function Profile() {
       }
     );
   };
+
+  const onHandleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart())
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json()
+      if (data?.success === false) {
+        dispatch(signInFailure(data))
+        setUpdateSuccess(false)
+        return;
+      }
+      dispatch(signInSuccess(data))
+      setUpdateSuccess(true)
+    }
+    catch (error) {
+      dispatch(signInFailure(error));
+      setUpdateSuccess(false)
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input
           type='file'
           ref={fileRef}
@@ -69,11 +101,12 @@ export default function Profile() {
           )}
         </p>
         <input
-          defaultValue={currentUser.username}
+          defaultValue={currentUser.userName}
           type='text'
           id='username'
           placeholder='Username'
           className='bg-slate-100 rounded-lg p-3'
+          onChange={onHandleChange}
         />
         <input
           defaultValue={currentUser.email}
@@ -81,21 +114,26 @@ export default function Profile() {
           id='email'
           placeholder='Email'
           className='bg-slate-100 rounded-lg p-3'
+          onChange={onHandleChange}
         />
         <input
           type='password'
           id='password'
           placeholder='Password'
           className='bg-slate-100 rounded-lg p-3'
+          onChange={onHandleChange}
         />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          update
+        <button disabled={loader} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+          {loader ? "Loading..." : "update"}
         </button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+      <p className='text-red-700 mt-5'>{errorMsg ? errorMsg.message || "Something went wrong!!" : ""}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess && "Updated Successfully"}</p>
+
     </div>
   );
 }
